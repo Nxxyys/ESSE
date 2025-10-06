4f5e67ff4c953085a25b3de18674f44f969006c6
Nxxyys <ilhamazharrafinsky@gmail.com>
2025-10-06 23:31:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ onload = () => {
     document.body.classList.remove("not-loaded");
 
     const titles = ('I LOVE U').split('')
-     const titles = ('ESTEPHANIA VALERIE GRACIA').split('')
+    const titles = ('ESTEPHANIA VALERIE GRACIA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
